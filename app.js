b97e8bfc71bb3494901903fb3ee74aca4df211c0
dvthnight <davide.lanotte@protonmail.com>
2022-02-14 15:39:29 +0100

    iniziato a competare la funzione palindorma

---
+++ app.js
@@ -0,0 +1,59 @@
+/*
+Palidroma
+Chiedere all’utente di inserire una parola 
+Creare una funzione per capire se la parola inserita
+è palindroma
+*/
+
+function palindroma (array){
+
+    let meta = Math.floor(array.length/2);
+    console.log(array, array.length,meta);
+    let j= array.length-1;
+    let controllo;
+
+    for(let i=0; i<meta; i++){
+        console.log(array[i], array[j])
+        if(array[i]==array[j]){
+            controllo = true;
+        }else{
+            console = false;
+            break;
+        }
+        j--;
+    }
+
+    return controllo;
+}
+
+const parola = prompt("inserisci una parola");
+const arrayParola = [...parola];
+console.log(parola, arrayParola);
+
+const risposta = palindroma (arrayParola);
+
+console.log(risposta);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce 
+un numero da 1 a 5. Generiamo un numero random 
+(sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri Stabiliamo se la somma dei due numeri 
+è pari o dispari (usando una funzione) Dichiariamo
+chi ha vinto.
+*/
